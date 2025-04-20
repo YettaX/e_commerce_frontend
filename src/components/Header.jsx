18ba2875ecc4079652bc15d9e7c7d902097c2956
YettaX <arrleo@163.com>
2025-04-20 17:40:01 +1000

    add a left drawer with shipping info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import CategoryMenu from "./CategoryMenu";
 import {
-  PlusOutlined
+  CloseOutlined
 } from '@ant-design/icons';
 
 export default function Header() {
@@ -21,28 +21,61 @@ export default function Header() {
         Receive a complimentary Aesop Face Cloth with all online orders over $150. Excludes Click and Collect.  &nbsp;&nbsp;&nbsp;+
       </div>
       <div>
-      <CategoryMenu />
-    </div>
+        <CategoryMenu />
+      </div>
 
-     
+
       {isModalVisible && (
-        <div className="fixed top-0 left-0 bg-[#fffef2] text-white p-4 w-64 h-full flex flex-col justify-between">
-          <div>
-            <h3 className="font-bold text-lg">Special Offer</h3>
-            <p>
-              Get a complimentary Aesop Face Cloth with all online orders over $150. Excludes Click and Collect.
+        <div className="fixed top-0 left-0 bg-[#f6f5e8] p-4 w-6/12 h-full flex flex-col z-50">
+
+          {/** Shipping title */}
+          <div className="h-20 px-20 font-sans  flex justify-between items-center">
+            <p className="text-[20px] leading-[34px] font-normal">
+              Shipping fees and delivery times
             </p>
+            <button onClick={toggleModal}>
+              <CloseOutlined />
+            </button>
           </div>
-          <button
-            className="bg-red-500 text-white p-2 rounded mt-4"
-            onClick={toggleModal}
-          >
-            Close
-          </button>
+
+          {/** Shipping info */}
+          <div className='overflow-y-auto '>
+
+            <div className='border-t mx-20 py-10 '>
+              <p className='desc_title'>Standard</p>
+              <div className='flex justify-between mb-2 description'>
+                <span >All orders</span>
+                <span>Complimentary</span>
+              </div>
+              <div className='flex justify-between mb-2 description'>
+                <span >Metro areas</span>
+                <span>3-5 business days</span>
+              </div>
+              <div className='flex justify-between mb-2 description'>
+                <span >Most other areas</span>
+                <span>2–7 business days</span>
+              </div>
+
+            </div>
+
+            <div className='border-t mx-20 py-10'>
+              <p className='desc_title'>Same day</p>
+              <div className='flex justify-between mb-4 description'>
+                <span >All orders</span>
+                <span>$14</span>
+              </div>
+              
+              <p className='description'>Melbourne, Sydney & Perth metro areas</p>
+              <p className='description'>Same day delivery when ordered by 12pm on weekdays</p>
+              <p className='description'>(excludes public holidays)</p>
+             
+
+            </div>
+
+          </div>
         </div>
       )}
     </>
   );
 }
 
-  
